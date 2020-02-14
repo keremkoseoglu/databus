bc21608a2fb295eadaf8f979d6612e3bd767565b
Kerem Koseoglu <kerem@koseoglu.info>
2020-02-14 11:46:27 +0300

    Databüs kaba plan

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
 print("Hello world!")
+
+# todo: Log
+# log sınıfı aç, timestamp ile birlikte log yazsın
+# her cycle'da yeni log dosyası açsın
+# log saklama süresini geçen dosyaları silecek yordam
+
+# todo: 3P
+# PULL klasörü: abstract + uygulama sınıfları
+# PROCESS klasörü: abstract + uygulama sınıfları
+# PUSH klasörü: abstract + uygulama sınıfları
+# tüm bu sınıflar loga ek yapabilsin
+# bunların parametreleri aynı klasörde mi olsun merkezi yerde mi?
+
+# todo: passenger
+# PASSENGER klasörü
+# senkronizasyon sıklığı
+# JSON: passenger tanımları
+# JSON: PULL, PROCESS, PUSH listesi
+
+# todo: driver
+# konfigürasyon dosyası
+# - log saklama süresi
+# işlem
+# - tüm klasörlerden elindeki bilgileri tazele
+# - passenger'lardan cycle'ı gelenler için 3P
+# - log kaydet
+# - eski log'ları sil (konfigürasyondaki süreyi geçenler)
+# - yeni cycle
