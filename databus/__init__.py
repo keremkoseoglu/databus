a5ef0ed4f5dd579ffeab3aefd499310d6164a048
keremkoseoglu <kerem@keremkoseoglu.com>
2020-05-22 18:45:01 +0300

    requirements.txt bug fix

--- databus/__init__.py
+++ databus/__init__.py
@@ -2,7 +2,7 @@
 from os import path
 from sys import modules
 
-__version__ = "4.5.0"
+__version__ = "4.5.1"
 AUTHOR = "Kerem Koseoglu"
 EMAIL = "kerem@keremkoseoglu.com"
 DESCRIPTION = "Databus is a framework to transfer data between systems."
