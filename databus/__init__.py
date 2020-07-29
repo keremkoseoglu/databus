252fdd00ae40e484fb69450533fe18926704c4fa
keremkoseoglu <kerem@keremkoseoglu.com>
2020-07-29 09:23:43 +0300

    Version increase

--- databus/__init__.py
+++ databus/__init__.py
@@ -2,7 +2,7 @@
 from os import path
 from sys import modules
 
-__version__ = "4.6.4"
+__version__ = "4.6.5"
 AUTHOR = "Kerem Koseoglu"
 EMAIL = "kerem@keremkoseoglu.com"
 DESCRIPTION = "Databus is a framework to transfer data between systems."
