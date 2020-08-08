64c9b2a606db4efc98cde0c0ca89853693694199
keremkoseoglu <kerem@keremkoseoglu.com>
2020-08-08 21:39:18 +0300

    beautify json on customizing edit Fixes #72

--- databus/__init__.py
+++ databus/__init__.py
@@ -2,7 +2,7 @@
 from os import path
 from sys import modules
 
-__version__ = "5.1.2"
+__version__ = "5.2.0"
 AUTHOR = "Kerem Koseoglu"
 EMAIL = "kerem@keremkoseoglu.com"
 DESCRIPTION = "Databus is a framework to transfer data between systems."
