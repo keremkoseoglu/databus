f25e7c300721124ee3d0c319ecfedd7ae5ed7d49
keremkoseoglu <kerem@keremkoseoglu.com>
2020-11-15 14:06:04 +0300

    show polite message on peek error Fixes #87

--- databus/__init__.py
+++ databus/__init__.py
@@ -2,7 +2,7 @@
 from os import path
 from sys import modules
 
-__version__ = "6.8.0"
+__version__ = "6.8.3"
 AUTHOR = "Kerem Koseoglu"
 EMAIL = "kerem@keremkoseoglu.com"
 DESCRIPTION = "Databus is a framework to transfer data between systems."
