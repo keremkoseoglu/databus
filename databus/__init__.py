0760a17cf90affc266f0b952116fa9f294e9dc78
keremkoseoglu <kerem@keremkoseoglu.com>
2021-01-28 18:58:49 +0300

    hata olursa başka maillere devam etsin Fixes #131

--- databus/__init__.py
+++ databus/__init__.py
@@ -2,7 +2,7 @@
 from os import path
 from sys import modules
 
-__version__ = "7.0.3"
+__version__ = "7.0.6"
 AUTHOR = "Kerem Koseoglu"
 EMAIL = "kerem@keremkoseoglu.com"
 DESCRIPTION = "Databus is a middleware framework"
