c9ed1904e383ee2c297ccc9fa613f0c4dd8845b8
Dr. Kerem Koseoglu <kerem@keremkoseoglu.com>
2021-04-13 11:00:52 +0300

    turkish character problem on extracted zip files Fixes #146

--- build/lib/databus/passenger/abstract_passenger.py
+++ build/lib/databus/passenger/abstract_passenger.py
@@ -134,7 +134,7 @@ class AbstractPassenger(ABC): # pylint: disable=R0903, R0902
                 file_in_zip = os.path.split(extracted_path)[1]
 
                 if file_format == AttachmentFormat.text:
-                    with open(extracted_path, "r") as extracted_file:
+                    with open(extracted_path, "r", encoding="utf-8") as extracted_file:
                         file_content = extracted_file.read()
                         file_content = file_content.replace("\n", " ")
                         file_content = file_content.replace("\t", " ")
